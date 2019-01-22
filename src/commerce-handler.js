75b42575371b70b55e1a11253a6e9c75b4ca2752
Robert Ing <robertming@gmail.com>
2019-01-22 14:55:39 -0500

    Implement commerce-handler

--- src/commerce-handler.js
+++ src/commerce-handler.js
@@ -1,63 +1,42 @@
+var common = require('./common'),
+    TRANSACTIONS = 'transactions',
+    ITEMS_SOLD = 'items_sold';
+
 var commerceHandler = {
     logCommerceEvent: function(event) {
-        /*
-        Sample ecommerce event schema:
-        {
-            CurrencyCode: 'USD',
-            DeviceId:'a80eea1c-57f5-4f84-815e-06fe971b6ef2', // MP generated
-            EventAttributes: { key1: 'value1', key2: 'value2' },
-            EventType: 16,
-            EventCategory: 10, // (This is an add product to cart event, see below for additional ecommerce EventCategories)
-            EventName: "eCommerce - AddToCart",
-            MPID: "8278431810143183490",
-            ProductAction: {
-                Affiliation: 'aff1',
-                CouponCode: 'coupon',
-                ProductActionType: 7,
-                ProductList: [
-                    {
-                        Attributes: { prodKey1: 'prodValue1', prodKey2: 'prodValue2' },
-                        Brand: 'Apple',
-                        Category: 'phones',
-                        CouponCode: 'coupon1',
-                        Name: 'iPhone',
-                        Price: '600',
-                        Quantity: 2,
-                        Sku: "SKU123",
-                        TotalAmount: 1200,
-                        Variant: '64GB'
+        if (event.EventDataType === 16) {
+            var eventMapping = common.getEventMapping(event);
+            if (eventMapping && eventMapping.result && eventMapping.match) {
+                var gtagProperties = {};
+                common.setCustomVariables(event, gtagProperties);
+                common.setSendTo(eventMapping.match, event.CustomFlags, gtagProperties);
+                if (event.CustomFlags && event.CustomFlags['DoubleClick.Counter']) {
+                    gtagProperties.send_to += ('+' + event.CustomFlags['DoubleClick.Counter']);
+                    //stringify number
+                    gtagProperties.value = '' + event.ProductAction.TotalAmount;
+                    gtagProperties.transaction_id = event.ProductAction.TransactionId;
+
+                    if (event.CustomFlags['DoubleClick.Counter'] === TRANSACTIONS) {
+                        common.sendGtag('purchase', gtagProperties);
+                    } else if (event.CustomFlags['DoubleClick.Counter'] === ITEMS_SOLD) {
+                        //stringify number
+                        gtagProperties.quantity = '' + event.ProductAction.ProductList.length;
+
+                        common.sendGtag('purchase', gtagProperties);
+                    } else {
+                        console.log('Counter type not valid. For sales conversions, use \'transactions\', or \'items_sold\'. See https://support.google.com/dcm/partner/answer/2823400?hl=en for more info')
+                        return false;
                     }
-                ],
-                TransactionId: "tid1",
-                ShippingAmount: 10,
-                TaxAmount: 5,
-                TotalAmount: 1215,
-            },
-            UserAttributes: { userKey1: 'userValue1', userKey2: 'userValue2' }
-            UserIdentities: [
-                {
-                    Identity: 'test@gmail.com', Type: 7
+                } else {
+                    console.log('Counter type not valid. For sales conversions, use \'transactions\', or \'items_sold\'. See https://support.google.com/dcm/partner/answer/2823400?hl=en for more info')
+                    return false;
                 }
-            ]
+                window.gtag('event', 'purchase', gtagProperties);
+            } else {
+                console.log('Event not mapped. Event not sent.');
+                return false;
+            }
         }
-
-        If your SDK has specific ways to log different eCommerce events, see below for
-        mParticle's additional ecommerce EventCategory types:
-
-            10: ProductAddToCart, (as shown above)
-            11: ProductRemoveFromCart,
-            12: ProductCheckout,
-            13: ProductCheckoutOption,
-            14: ProductClick,
-            15: ProductViewDetail,
-            16: ProductPurchase,
-            17: ProductRefund,
-            18: PromotionView,
-            19: PromotionClick,
-            20: ProductAddToWishlist,
-            21: ProductRemoveFromWishlist,
-            22: ProductImpression
-        */
     }
 };
 
