cea6d63914688230d147b8fe1909d00c7853c978
Alexander Sapountzis <alexs@mparticle.com>
2019-06-25 11:18:23 -0400

    Add backwards compatible build folder

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,16 @@ export default [
         output: {
             ...base.output,
             file: 'dist/DoubleClick-Kit.js',
-            name: 'mp-doubleclick-kit'
+            name: 'mp-doubleclick-kit',
+        },
+        plugins: [...base.plugins]
+    },
+    {
+        input: base.input,
+        output: {
+            ...base.output,
+            file: 'build/DoubleClick-Kit.js',
+            name: 'mp-doubleclick-kit',
         },
         plugins: [...base.plugins]
     }
